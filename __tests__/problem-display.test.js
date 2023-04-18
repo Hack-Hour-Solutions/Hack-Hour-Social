06a844a16488bd4c25aa8c6e1eb824041cfe001c
erikahjung <erika.h.jung@gmail.com>
2023-04-18 15:50:46 -0700

    TDD ProblemDisplay bug fixes

--- __tests__/problem-display.test.js
+++ __tests__/problem-display.test.js
@@ -9,27 +9,26 @@ import '@testing-library/jest-dom';
 import ProblemDisplay from '../src/components/ProblemDisplay';
 
 describe('Title', () => {
-  let display;
   const props = {
     title: 'Two Sum',
-    link: 'https://leetcode.com/problems/two-sum/',
+    url: 'https://leetcode.com/problems/two-sum/',
     difficulty: 'Easy'
   }
 
-  beforeAll(() => {
-    display = render(<ProblemDisplay {...props} />)
+  beforeEach(() => {
+    render(<ProblemDisplay {...props} />)
   })
 
   it('Renders a header', () => {
-    expect(display.getByRole('heading')).toHaveTextContent('Today\'s Hack Hour');
+    expect(screen.getByRole('heading')).toHaveTextContent('Today\'s Hack Hour');
   })
 
-  // it('Renders title, link, and difficulty level of the problem', () => {
-  //   expect(display.getByText('Title:')).toHaveStyle('font-weight: bold');
-  //   expect(display.getByText('Title:').nextSibling).toHaveTextContent('Two Sum');
-  //   expect(display.getByText('Link:')).toHaveStyle('font-weight: bold');
-  //   expect(display.getByRole('link')).toHaveAttribute('href', 'https://leetcode.com/problems/two-sum/');
-  //   expect(display.getByText('Difficulty:')).toHaveStyle('font-weight: bold');
-  //   expect(display.getByText('Difficulty:').nextSibling).toHaveTextContent('Easy');
-  // })
-})
+  it('Renders title, link, and difficulty level of the problem', () => {
+    expect(screen.getByText('Title:')).toHaveStyle('font-weight: bold');
+    expect(screen.getByText('Title:').nextSibling).toHaveTextContent('Two Sum');
+    expect(screen.getByText('Link:')).toHaveStyle('font-weight: bold');
+    expect(screen.getByRole('link')).toHaveAttribute('href', 'https://leetcode.com/problems/two-sum/');
+    expect(screen.getByText('Difficulty:')).toHaveStyle('font-weight: bold');
+    expect(screen.getByText('Difficulty:').nextSibling).toHaveTextContent('Easy');
+  })
+});
