41a5807aee0d6e5b736051e7cfefe936c63e3f37
Sam Henderson <shendo87@gmail.com>
2023-04-17 19:07:44 -0700

    Modify babel config; add some modules

---
+++ server/__tests__/database.test.js
@@ -0,0 +1,11 @@
+import controller from '../controllers'
+
+describe('post.controller connects to database and retrieves posts', () => {
+  
+  it ('Connects to database', () => {
+    // const req = {};
+    // const res = { render: jest.fn()};
+    // controller.post(req, res)
+    return true;
+  })
+})
